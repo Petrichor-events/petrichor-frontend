3142081d9594f5c9e6ca7acda115f617d641482e
Karthik_Krishna_M_CS <168297968+KarthikKrishnaMCS@users.noreply.github.com>
2025-06-07 11:59:44 +0530

    Updated Login.svelte
    Created the login component

--- src/lib/shared/ui/components/login/Login.svelte
+++ src/lib/shared/ui/components/login/Login.svelte
@@ -1,6 +1,27 @@
 <script>
+    import './Login.css'
+    let username="";
+    let password="";
+    const onEnter = () => {
+        console.log("Hi",username,password)
+    }
 </script>
+<main>
+	<div class="container">
+		<div class="login">
+			<h2>Login</h2>
+			<input type="text" placeholder="Username" bind:value={username}>
+			<input type="text" placeholder="Password" bind:value={password}>
+			<div class="redirect">
+				<a href="#" id="link1">Register Instead</a>
+				<a href="#" id="link2">Forgot Password</a>
+			</div>
+			<button class="login-btn" on:click={onEnter}>Login</button>
+		</div>
+	</div>
+</main>
+<style>
 
-<form data-testid="login">
-	<p>This is a test login form</p>
-</form>
+	
+</style>
+
